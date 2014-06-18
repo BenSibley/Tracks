17954c7499fe78cb47d8784649abde430e5d6c10
BenSibley <ben@competethemes.com>
2014-06-18 13:06:18 -0400

    added description at wide screen sizes and updated secondary nav styling

--- menu-primary.php
+++ menu-primary.php
@@ -3,7 +3,9 @@
 <div id="menu-primary-tracks" class="menu-primary-tracks"></div>
 <div id="menu-primary" class="menu-container menu-primary" role="navigation">
 
-    <p class="site-description"><?php bloginfo('description'); ?></p>
+    <?php if(get_bloginfo('description')){ ?>
+        <p class="site-description"><?php bloginfo('description'); ?></p>
+    <?php } ?>
 
     <?php wp_nav_menu( array( 'theme_location' => 'primary', 'container_class' => 'menu', 'menu_class' => 'menu-primary-items', 'menu_id' => 'menu-primary-items', 'items_wrap' => '<ul id="%1$s" class="%2$s" role="menubar">%3$s</ul>', 'fallback_cb' => 'ct_tracks_wp_page_menu') ); ?>
 
