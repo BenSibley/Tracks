fdf55a1d192dd938fb3cd5691092e7cfeb433415
BenSibley <ben@competethemes.com>
2014-06-23 18:45:14 -0400

    removed leftover commented code

--- functions.php
+++ functions.php
@@ -310,23 +310,6 @@ function ct_tracks_author_social_icons() {
     // array of social media site names
     $social_sites = ct_tracks_social_array();
 
-    /*
-    foreach($social_sites as $key => $social_site) {
-    	if(get_the_author_meta( $social_site)) {
-    		if($key == 'googleplus') {
-				echo "<a href='".esc_url(get_the_author_meta( $social_site))."'><i class=\"fa fa-google-plus-square\"></i></a>";
-			} elseif($key == 'flickr') {
-				echo "<a href='".esc_url(get_the_author_meta( $social_site))."'><i class=\"fa fa-flickr\"></i></a>";
-			} elseif($key == 'dribbble') {
-                echo "<a href='".esc_url(get_the_author_meta( $social_site))."'><i class=\"fa fa-dribbble\"></i></a>";
-            } elseif($key == 'instagram') {
-                echo "<a href='".esc_url(get_the_author_meta( $social_site))."'><i class=\"fa fa-instagram\"></i></a>";
-            } else {
-	    		echo "<a href='".esc_url(get_the_author_meta( $social_site))."'><i class=\"fa fa-$key-square\"></i></a>";
-	    	}
-    	}
-    }
-*/
     foreach ($social_sites as $key => $social_site) {
         if(get_the_author_meta( $social_site)) {
             if( $key ==  "flickr" || $key ==  "dribbble" || $key ==  "instagram" || $key ==  "soundcloud" || $key ==  "spotify" || $key ==  "vine" || $key ==  "yahoo" || $key ==  "codepen" || $key ==  "delicious" || $key ==  "stumbleupon" || $key ==  "deviantart" || $key ==  "digg" || $key ==  "hacker-news") {
