bfeef4f015f4cab3778e7b2165c02b27a693a770
BenSibley <ben@competethemes.com>
2014-06-30 11:11:56 -0400

    removed JS function for two-column layout.

--- js/functions.js
+++ js/functions.js
@@ -140,23 +140,6 @@ jQuery(document).ready(function($){
     }
     displayLayoutOptions();
 
-    // ===== Two-Column Layout ==== //
-
-    function removeLayoutGaps(){
-
-        $('.excerpt').each(function(){
-
-            // 40% of the screen over to be safe
-            var windowWidth = $(window).width() * 0.4;
-
-            // if it ends of over on the right, float it right
-            if($(this).offset().left > windowWidth){
-                $(this).css('float','right');
-            }
-        });
-    }
-    removeLayoutGaps();
-
 });
 
 /* fix for skip-to-content link bug in Chrome & IE9 */
