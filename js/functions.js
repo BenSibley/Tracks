9222b5abfefe3cb0da39979fe17179623cfe5622
BenSibley <ben@competethemes.com>
2014-06-30 11:13:49 -0400

    removed JS function to show/hide additional premium layout options.

--- js/functions.js
+++ js/functions.js
@@ -118,28 +118,6 @@ jQuery(document).ready(function($){
         }
     }
 
-    // ===== Show/Hide Customizer Options ==== //
-
-    function displayLayoutOptions(){
-
-        var imageHeightOption = $('html', window.parent.document).find('#customize-control-premium_layouts_full_width_image_height');
-        var fullPostOption = $('html', window.parent.document).find('#customize-control-premium_layouts_full_width_full_post');
-
-        imageHeightOption.hide();
-        fullPostOption.hide();
-
-        // if the layout is set to full-width images, display the image height option
-        $('html', window.parent.document).find('#customize-control-premium_layouts_setting option').each(function(){
-            if($(this).attr('selected') == 'selected' && $(this).val() == 'full-width-images'){
-                imageHeightOption.show();
-            }
-            if($(this).attr('selected') == 'selected' && $(this).val() == 'full-width'){
-                fullPostOption.show();
-            }
-        });
-    }
-    displayLayoutOptions();
-
 });
 
 /* fix for skip-to-content link bug in Chrome & IE9 */
