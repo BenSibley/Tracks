6160b579a7190efc7eae0951672278bd341e81ab
BenSibley <ben@competethemes.com>
2014-06-30 11:38:02 -0400

    reinstalled grunt. removed hybrid gallery.min.css from enqueueing and added those styles to style.css

--- functions.php
+++ functions.php
@@ -54,7 +54,6 @@ function ct_tracks_theme_setup() {
 	/* Theme-supported features go here. */
     add_theme_support( 'hybrid-core-template-hierarchy' );
     add_theme_support( 'loop-pagination' );
-    add_theme_support( 'cleaner-gallery' );
     add_theme_support( 'hybrid-core-widgets' );
 
     // from WordPress core not theme hybrid
