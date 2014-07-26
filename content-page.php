de6add352e498092f646f373c4140b35a3a06933
BenSibley <ben@competethemes.com>
2014-07-26 19:04:26 -0400

    fixed page with featured image layout for two-column-images and full-width-images

--- content-page.php
+++ content-page.php
@@ -1,5 +1,15 @@
 <div class='entry'>
-    <?php ct_tracks_featured_image(); ?>
+    <?php
+    if(get_theme_mod('premium_layouts_setting') == 'full-width-images' || get_theme_mod('premium_layouts_setting') == 'two-column-images'){
+        if (has_post_thumbnail( $post->ID ) ) {
+            echo "<div class='featured-image-container'>";
+            ct_tracks_featured_image();
+            echo "</div>";
+        }
+    } else {
+        ct_tracks_featured_image();
+    }
+    ?>
 	<div class='entry-header'>
             <h1 class='entry-title'><?php the_title(); ?></h1>
     </div>
