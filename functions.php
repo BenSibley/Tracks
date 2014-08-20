a6d8afc24adc0d75c1eec4d4609b71ee40be2842
BenSibley <ben@competethemes.com>
2014-08-20 10:52:01 -0400

    now enqueueing profile-uploader on user edit pages

--- functions.php
+++ functions.php
@@ -43,7 +43,7 @@ add_action('admin_enqueue_scripts',	'ct_tracks_enqueue_admin_styles' );
 function ct_tracks_enqueue_profile_image_uploader($hook) {
 
     // if is user profile page
-    if('profile.php' == $hook){
+    if('profile.php' == $hook || 'user-edit.php'){
 
         // Enqueues all scripts, styles, settings, and templates necessary to use all media JavaScript APIs.
         wp_enqueue_media();
