54d6def188c70bc6641b2507933d5e9fe1ecc761
BenSibley <ben@competethemes.com>
2014-09-16 09:51:40 -0400

    fixed archives not showing when bbpress active

--- index.php
+++ index.php
@@ -97,6 +97,10 @@ if ( have_posts() ) :
                 if( is_bbpress() ) {
                     get_template_part( 'content/bbpress' );
                 }
+                /* normal archive */
+                else {
+                    get_template_part('content');
+                }
             }
             elseif(get_theme_mod('premium_layouts_setting') == 'two-column-images'){
                 get_template_part('licenses/content/content-two-column-images');
