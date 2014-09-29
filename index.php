442fc96e8578219a8e75853a36381340997f84b3
BenSibley <ben@competethemes.com>
2014-09-29 12:07:19 -0400

    i18n for index.php

--- index.php
+++ index.php
@@ -8,21 +8,21 @@ $comments_display = get_theme_mod('ct_tracks_comments_setting');
 /* Category header */
 if(is_category()){ ?>
     <div class='archive-header'>
-    <p>Category:</p>
+    <p><?php _e('Category:', 'tracks'); ?></p>
     <h2><?php single_cat_title(); ?></h2>
     </div><?php
 }
 /* Tag header */
 elseif(is_tag()){ ?>
     <div class='archive-header'>
-    <p>Tag:</p>
+    <p><?php _e('Tag:', 'tracks'); ?></p>
     <h2><?php single_tag_title(); ?></h2>
     </div><?php
 }
 /* Author header */
 elseif(is_author()){ ?>
     <div class='archive-header'>
-    <p>These Posts are by:</p>
+    <p><?php _e('These Posts are by:', 'tracks'); ?></p>
     <h2><?php echo get_the_author(); ?></h2>
     </div><?php
 }
