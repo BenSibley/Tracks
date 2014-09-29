16fdaca1ccb77d82f2022b2457d9c651ac147c0e
BenSibley <ben@competethemes.com>
2014-09-29 12:32:46 -0400

    i18n for theme options page

--- theme-options.php
+++ theme-options.php
@@ -7,28 +7,25 @@ function ct_tracks_register_theme_page(){
 add_action( 'admin_menu', 'ct_tracks_register_theme_page' );
 
 /* callback used to add content to options page */
-function ct_tracks_options_content(){
-
-    ?>
+function ct_tracks_options_content(){ ?>
 
     <div id="tracks-dashboard-wrap" class="wrap">
-
-        <h2>Tracks Dashboard</h2>
+        <h2><?php _e('Tracks Dashboard', 'tracks'); ?></h2>
 
         <?php $active_tab = isset( $_GET[ 'tab' ] ) ? $_GET[ 'tab' ] : 'getting-started'; ?>
 
         <h2 class="nav-tab-wrapper">
-            <a href="?page=tracks-options&tab=getting-started" class="nav-tab <?php echo $active_tab == 'getting-started' ? 'nav-tab-active' : ''; ?>">Getting Started</a>
-            <a href="?page=tracks-options&tab=support" class="nav-tab <?php echo $active_tab == 'support' ? 'nav-tab-active' : ''; ?>">Support</a>
-            <a href="?page=tracks-options&tab=premium-upgrades" class="nav-tab <?php echo $active_tab == 'premium-upgrades' ? 'nav-tab-active' : ''; ?>">Premium Upgrades</a>
-            <a href="?page=tracks-options&tab=licenses" class="nav-tab <?php echo $active_tab == 'licenses' ? 'nav-tab-active' : ''; ?>">Licenses</a>
+            <a href="?page=tracks-options&tab=getting-started" class="nav-tab <?php echo $active_tab == 'getting-started' ? 'nav-tab-active' : ''; ?>"><?php _e('Getting Started', 'tracks'); ?></a>
+            <a href="?page=tracks-options&tab=support" class="nav-tab <?php echo $active_tab == 'support' ? 'nav-tab-active' : ''; ?>"><?php _e('Support', 'tracks'); ?></a>
+            <a href="?page=tracks-options&tab=premium-upgrades" class="nav-tab <?php echo $active_tab == 'premium-upgrades' ? 'nav-tab-active' : ''; ?>"><?php _e('Premium Upgrades', 'tracks'); ?></a>
+            <a href="?page=tracks-options&tab=licenses" class="nav-tab <?php echo $active_tab == 'licenses' ? 'nav-tab-active' : ''; ?>"><?php _e('Licenses', 'tracks'); ?></a>
         </h2>
         <?php
         if($active_tab == 'getting-started'){ ?>
             <div class="content-getting-started content">
                 <h3><?php _e('Start Customizing', 'tracks'); ?></h3>
                 <p><?php _e('Thanks for downloading Tracks!', 'tracks'); ?></p>
-                <p><?php _e("To start customizing Tracks, click the 'Customize' option in your menu, or use the button below to get started", 'tracks'); ?>.</p>
+                <p><?php _e('To start customizing Tracks, click the "Customize" link to the left in your menu, or use the button below to get started', 'tracks'); ?>.</p>
                 <p>
                     <a class="button-primary" href="customize.php"><?php _e('Use Customizer', 'tracks') ?></a>
                 </p>
@@ -45,21 +42,21 @@ function ct_tracks_options_content(){
         <?php }
         elseif($active_tab == 'premium-upgrades'){ ?>
             <div class="content-premium-upgrades content">
-                <h3>Premium Upgrades ($9-15)</h3>
-                <p>New layouts, features, and more. Make your site more customizable and beautiful with Tracks premium upgrades.</p>
-                <p><a target="_blank" class="button-primary" href="http://www.competethemes.com/tracks-theme-upgrades/">Visit Upgrades Gallery</a></p>
+                <h3><?php _e('Premium Upgrades ($9-15)', 'tracks'); ?></h3>
+                <p><?php _e('New layouts, features, and more. Make your site more customizable and beautiful with Tracks premium upgrades', 'tracks');?>.</p>
+                <p><a target="_blank" class="button-primary" href="http://www.competethemes.com/tracks-theme-upgrades/"><?php _e('Visit Upgrades Gallery', 'tracks'); ?></a></p>
             </div>
         <?php }
         elseif($active_tab == 'licenses'){ ?>
             <div class="content-licenses content">
-                <h3>Premium Layouts</h3>
+                <h3><?php _e('Premium Layouts', 'tracks'); ?></h3>
                 <?php
                 // array of available layouts
                 $layouts = array('two_column', 'two_column_images', 'full_width', 'full_width_images');
                 // create form for each layout
                 ct_tracks_license_form_output($layouts);
                 ?>
-                <h3>Premium Features</h3>
+                <h3><?php _e('Premium Features', 'tracks'); ?></h3>
                 <?php
                 // array of available features
                 $features = array('background_images', 'background_textures', 'featured_videos');
@@ -86,33 +83,34 @@ function ct_tracks_license_form_output($upgrades){
             <?php settings_fields('ct_tracks_' . $upgrade . '_license'); ?>
             <h4>
                 <?php
+                // No i18n because product names are Proper Names
                 if($upgrade == 'two_column'){
-                    _e('Two-Column Layout','tracks');
+                    echo 'Two-Column Layout';
                 }
                 elseif($upgrade == 'two_column_images'){
-                    _e('Two-Column Images Layout','tracks');
+                    echo 'Two-Column Images Layout';
                 }
                 elseif($upgrade == 'full_width'){
-                    _e('Full-width Layout','tracks');
+                    echo 'Full-width Layout';
                 }
                 elseif($upgrade == 'full_width_images'){
-                    _e('Full-width Images Layout','tracks');
+                    echo 'Full-width Images Layout';
                 }
                 elseif($upgrade == 'background_images'){
-                    _e('Background Images','tracks');
+                    echo 'Background Images';
                 }
                 elseif($upgrade == 'background_textures'){
-                    _e('Background Textures','tracks');
+                    echo 'Background Textures';
                 }
                 elseif($upgrade == 'featured_videos'){
-	                _e('Featured Videos','tracks');
+	                echo 'Featured Videos';
                 }
                 ?>
             </h4>
             <table class="form-table">
                 <tbody>
                 <tr valign="top">
-                    <th scope="row" valign="top">Save License</th>
+                    <th scope="row" valign="top"><?php _e('Save License', 'tracks'); ?></th>
                     <td>
                         <input id="ct_tracks_<?php echo $upgrade; ?>_license_key" name="ct_tracks_<?php echo $upgrade; ?>_license_key" type="text" class="regular-text" placeholder="ex. 1d58ag920zf5e551bab24aa3d18e2c79" value="<?php echo esc_attr( $license ); ?>" />
                         <label class="description" for="ct_tracks_<?php echo $upgrade; ?>_license_key"><?php _e('Enter your license key','tracks'); ?></label>
@@ -137,14 +135,18 @@ function ct_tracks_license_form_output($upgrades){
                 </tbody>
             </table>
             <?php if( $status !== false && $status == 'valid' ) {
+
+	            // set variable for customizer url
+	            $customizer_url = 'customize.php';
+
                 if($upgrade == 'background_images'){ ?>
-                    <p class="valid">You can add a background image now in the "Background Image" section in the <a href="customize.php">Customizer</a></p><?php }
+                    <p class="valid"><?php printf( __('You can now add a background image using the "Background Image" section in the <a href="%s">Customizer</a>', 'tracks'), esc_url( $customizer_url ) ); ?>.</p><?php }
                 elseif($upgrade == 'background_textures'){ ?>
-                    <p class="valid">You can add a background texture now in the "Background Texture" section in the <a href="customize.php">Customizer</a></p><?php }
+                    <p class="valid"><?php printf( __('You can now add a background texture using the "Background Texture" section in the <a href="%s">Customizer</a>', 'tracks'), esc_url( $customizer_url ) ); ?>.</p><?php }
                 elseif($upgrade == 'featured_videos'){ ?>
-	                <p class="valid">You can add a video to a post now in the Post Editor</p><?php }
+	                <p class="valid"><?php _e('You can now add videos to posts. Use the Featured Videos box under the Post Editor to get started', 'tracks'); ?>.</p><?php }
                 else { ?>
-                    <p class="valid">You can switch to your new layout in the "Premium Layouts" section in the <a href="customize.php">Customizer</a></p><?php }
+                    <p class="valid"><?php printf( __('You can now switch to your new layout in the "Premium Layouts" section in the <a href="%s">Customizer</a>', 'tracks'), esc_url( $customizer_url ) ); ?>.</p><?php }
             } ?>
             <?php submit_button('Save License'); ?>
         </form>
