38ef7ac75123daeb920d4fe3bdd9402e4ccbfa6a
BenSibley <ben@competethemes.com>
2014-10-06 12:53:23 -0400

    added bold.php page template

---
+++ templates/bold.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Bold
+ */
+?>
+
+<?php get_header() ?>
+
+	<div class="bold-template">
+		<h1 class="heading">This is our Value Proposition</h1>
+		<h2 class="sub-heading">You're going to like this</h2>
+		<div class="description">
+			<p>Fusce vehicula dolor arcu, sit amet blandit dolor mollis nec. Donec viverra eleifend lacus,
+				Vitae ullamcorper metus.</p>
+		</div>
+		<a class="button" href="#">Tell Me More</a>
+	</div>
+	<div class="template-bg-image"></div>
+
+<?php get_footer() ?>
