feb220f3669feb718acab5e5f9ce095d027c6ce7
BenSibley <ben@competethemes.com>
2014-10-08 12:30:31 -0400

    added Heading options to Bold customizer

--- functions.php
+++ functions.php
@@ -767,6 +767,54 @@ function ct_tracks_remove_customizer_content( $wp_customize ) {
 			$wp_customize->remove_section( 'ct_tracks_social_icons' );
 			$wp_customize->remove_section( 'ct_tracks_search_input' );
 			$wp_customize->remove_section( 'ct_tracks_post_meta_display' );
+			$wp_customize->remove_section( 'ct_tracks_comments_display' );
+			$wp_customize->remove_section( 'ct-footer-text' );
+			$wp_customize->remove_section( 'ct-custom-css' );
+			$wp_customize->remove_section( 'ct_tracks_premium_layouts' );
+			$wp_customize->remove_section( 'ct_tracks_additional_options' );
+			$wp_customize->remove_section( 'ct_tracks_background_image' );
+			$wp_customize->remove_section( 'ct_tracks_background_texture' );
+			$wp_customize->remove_section( 'ct_tracks_header_color' );
+			$wp_customize->remove_section( 'nav' );
+			$wp_customize->remove_section( 'static_front_page' );
+			$wp_customize->remove_section( 'widgets' );
+			$wp_customize->remove_panel( 'widgets' );
+
+			// section
+			$wp_customize->add_section( 'ct_tracks_bold_heading', array(
+				'title'      => __( 'Heading', 'tracks' ),
+				'priority'   => 10,
+				'capability' => 'edit_theme_options'
+			) );
+			// setting - color
+			$wp_customize->add_setting( 'ct_tracks_bold_heading_color_setting', array(
+				'type'              => 'theme_mod',
+				'capability'        => 'edit_theme_options',
+//				'sanitize_callback' => 'esc_textarea',
+			) );
+			// control - color
+			$wp_customize->add_control( new WP_Customize_Color_Control(
+				$wp_customize, 'ct_tracks_bold_heading_color_setting', array(
+					'label'          => __( 'Color', 'tracks' ),
+					'section'        => 'ct_tracks_bold_heading',
+					'settings'       => 'ct_tracks_bold_heading_color_setting'
+				)
+			) );
+			// setting - font size
+			$wp_customize->add_setting( 'ct_tracks_bold_heading_size_setting', array(
+				'type'              => 'theme_mod',
+				'capability'        => 'edit_theme_options',
+//				'sanitize_callback' => 'esc_textarea',
+			) );
+			// control - font size
+			$wp_customize->add_control( new ct_tracks_number_input_control(
+				$wp_customize, 'ct_tracks_bold_heading_size_setting', array(
+					'label' => __('Font Size', 'tracks'),
+					'section' => 'ct_tracks_bold_heading',
+					'settings' => 'ct_tracks_bold_heading_size_setting',
+					'type' => 'number',
+				)
+			) );
 		}
 	}
 }
