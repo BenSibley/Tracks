e94153b2e50b22933f78fb80ed204b26674fb821
BenSibley <ben@competethemes.com>
2014-10-14 13:16:09 -0400

    no change

--- js/functions.js
+++ js/functions.js
@@ -322,16 +322,6 @@ jQuery(function($){
     }
     videoHeightAdjust();
 
-    function fixedPositionImageMobileSupport() {
-
-        $(window).scroll(function() {
-            var scrolledY = $(window).scrollTop();
-            $('#template-bg-image').css('top', scrolledY + 'px');
-        });
-    }
-
-    //fixedPositionImageMobileSupport();
-
 });
 
 jQuery(window).load(function(){
