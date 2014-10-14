90eb6391f962796c0161eb71ef6d2283af6a3966
BenSibley <ben@competethemes.com>
2014-10-14 18:39:56 -0400

    refactored bold.php file

--- templates/bold.php
+++ templates/bold.php
@@ -6,16 +6,24 @@
 // get page ID
 $post_id = $post->ID;
 
-// get all UGC from template meta boxes
-$heading = sanitize_text_field( get_post_meta( $post_id, 'ct_tracks_bold_heading_key', true ) );
-$sub_heading = sanitize_text_field( get_post_meta( $post_id, 'ct_tracks_bold_sub_heading_key', true ) );
-$description = sanitize_text_field( get_post_meta( $post_id, 'ct_tracks_bold_description_key', true ) );
-$button_one_text = sanitize_text_field( get_post_meta( $post_id, 'ct_tracks_bold_button_one_key', true ) );
-$button_one_link = esc_url_raw( get_post_meta( $post_id, 'ct_tracks_bold_button_one_link_key', true ) );
-$button_two_text = sanitize_text_field( get_post_meta( $post_id, 'ct_tracks_bold_button_two_key', true ) );
-$button_two_link = esc_url_raw( get_post_meta( $post_id, 'ct_tracks_bold_button_two_link_key', true ) );
-$bg_image = esc_url_raw( get_post_meta( $post_id, 'ct_tracks_bold_bg_image_key', true ) );
+// meta box ids
+$input_ids = array( 'heading', 'sub_heading', 'description', 'button_one', 'button_one_link', 'button_two', 'button_two_link', 'bg_image' );
 
+// create array to store user input
+$user_input = array();
+
+// for each meta box, add the user input to the $user_input array
+foreach( $input_ids as $input ) {
+
+	if( $input == 'button_one_link' || $input == 'button_two_link' || $input == 'bg_image' ) {
+		$user_input[$input] = esc_url_raw( get_post_meta( $post_id, 'ct_tracks_bold_' . $input . '_key', true ) );
+	} else {
+		$user_input[$input] = sanitize_text_field( get_post_meta( $post_id, 'ct_tracks_bold_' . $input . '_key', true ) );
+	}
+}
+
+// combine arrays ( heading => user input )
+$user_input = array_combine( $input_ids, $user_input );
 ?>
 
 <?php get_header() ?>
@@ -23,29 +31,29 @@ $bg_image = esc_url_raw( get_post_meta( $post_id, 'ct_tracks_bold_bg_image_key',
 	<div class="bold-template">
 		<div class="container">
 			<?php
-			if( $heading ) {
-				echo "<h1 class='heading'>" . sanitize_text_field($heading) . "</h1>";
+			if( $user_input['heading'] ) {
+				echo "<h1 class='heading'>" . sanitize_text_field( $user_input['heading'] ) . "</h1>";
 			}
-			if( $sub_heading ) {
-				echo "<h2 class='sub-heading'>" . sanitize_text_field($sub_heading) . "</h2>";
+			if( $user_input['sub_heading'] ) {
+				echo "<h2 class='sub-heading'>" . sanitize_text_field( $user_input['sub_heading'] ) . "</h2>";
 			}
-			if( $description ) {
+			if( $user_input['description'] ) {
 				echo "<div class='description'>";
-					echo "<p>" . sanitize_text_field($description) . "</p>";
+					echo "<p>" . sanitize_text_field( $user_input['description'] ) . "</p>";
 				echo "</div>";
 			}
-			if( $button_one_text ) {
-				echo "<a class='button button-one' href='" . esc_url($button_one_link) . "'>" . sanitize_text_field($button_one_text) . "</a>";
+			if( $user_input['button_one_link'] ) {
+				echo "<a class='button button-one' href='" . esc_url( $user_input['button_one_link'] ) . "'>" . sanitize_text_field( $user_input['button_one'] ) . "</a>";
 			}
-			if( $button_two_text ) {
-				echo "<a class='button button-two' href='" . esc_url($button_two_link) . "'>" . sanitize_text_field($button_two_text) . "</a>";
+			if( $user_input['button_two_link'] ) {
+				echo "<a class='button button-two' href='" . esc_url( $user_input['button_two_link'] ) . "'>" . sanitize_text_field( $user_input['button_two'] ) . "</a>";
 			}
 			?>
 		</div>
 	</div>
 	<?php
-	if( $bg_image ) {
-		echo "<div id='template-bg-image' class='template-bg-image' style='background-image: url(" . esc_url($bg_image) . ")'></div>";
+	if( $user_input['bg_image'] ) {
+		echo "<div id='template-bg-image' class='template-bg-image' style='background-image: url(" . esc_url( $user_input['bg_image'] ) . ")'></div>";
 	}
 	echo "<div id='template-overlay' class='template-overlay'></div>";
 	?>
