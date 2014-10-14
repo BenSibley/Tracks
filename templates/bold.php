67b31885165a9bd5cb1e7afcceec12d56f0de70b
BenSibley <ben@competethemes.com>
2014-10-14 13:23:02 -0400

    added sanitization/validation to bold template

--- templates/bold.php
+++ templates/bold.php
@@ -3,16 +3,18 @@
  * Template Name: Bold
  */
 
+// get page ID
 $post_id = $post->ID;
 
-$heading = get_post_meta( $post_id, 'ct_tracks_bold_heading_key', true );
-$sub_heading = get_post_meta( $post_id, 'ct_tracks_bold_sub_heading_key', true );
-$description = get_post_meta( $post_id, 'ct_tracks_bold_description_key', true );
-$button_one_text = get_post_meta( $post_id, 'ct_tracks_bold_button_one_key', true );
-$button_one_link = get_post_meta( $post_id, 'ct_tracks_bold_button_one_link_key', true );
-$button_two_text = get_post_meta( $post_id, 'ct_tracks_bold_button_two_key', true );
-$button_two_link = get_post_meta( $post_id, 'ct_tracks_bold_button_two_link_key', true );
-$bg_image = get_post_meta( $post_id, 'ct_tracks_bold_bg_image_key', true );
+// get all UGC from template meta boxes
+$heading = sanitize_text_field( get_post_meta( $post_id, 'ct_tracks_bold_heading_key', true ) );
+$sub_heading = sanitize_text_field( get_post_meta( $post_id, 'ct_tracks_bold_sub_heading_key', true ) );
+$description = sanitize_text_field( get_post_meta( $post_id, 'ct_tracks_bold_description_key', true ) );
+$button_one_text = sanitize_text_field( get_post_meta( $post_id, 'ct_tracks_bold_button_one_key', true ) );
+$button_one_link = esc_url_raw( get_post_meta( $post_id, 'ct_tracks_bold_button_one_link_key', true ) );
+$button_two_text = sanitize_text_field( get_post_meta( $post_id, 'ct_tracks_bold_button_two_key', true ) );
+$button_two_link = esc_url_raw( get_post_meta( $post_id, 'ct_tracks_bold_button_two_link_key', true ) );
+$bg_image = esc_url_raw( get_post_meta( $post_id, 'ct_tracks_bold_bg_image_key', true ) );
 
 ?>
 
@@ -22,28 +24,28 @@ $bg_image = get_post_meta( $post_id, 'ct_tracks_bold_bg_image_key', true );
 		<div class="container">
 			<?php
 			if( $heading ) {
-				echo "<h1 class='heading'>$heading</h1>";
+				echo "<h1 class='heading'>" . sanitize_text_field($heading) . "</h1>";
 			}
 			if( $sub_heading ) {
-				echo "<h2 class='sub-heading'>$sub_heading</h2>";
+				echo "<h2 class='sub-heading'>" . sanitize_text_field($sub_heading) . "</h2>";
 			}
 			if( $description ) {
 				echo "<div class='description'>";
-					echo "<p>$description</p>";
+					echo "<p>" . sanitize_text_field($description) . "</p>";
 				echo "</div>";
 			}
 			if( $button_one_text ) {
-				echo "<a class='button button-one' href='$button_one_link'>$button_one_text</a>";
+				echo "<a class='button button-one' href='" . esc_url($button_one_link) . "'>" . sanitize_text_field($button_one_text) . "</a>";
 			}
 			if( $button_two_text ) {
-				echo "<a class='button button-two' href='$button_two_link'>$button_two_text</a>";
+				echo "<a class='button button-two' href='" . esc_url($button_two_link) . "'>" . sanitize_text_field($button_two_text) . "</a>";
 			}
 			?>
 		</div>
 	</div>
 	<?php
 	if( $bg_image ) {
-		echo "<div id='template-bg-image' class='template-bg-image' style='background-image: url($bg_image)'></div>";
+		echo "<div id='template-bg-image' class='template-bg-image' style='background-image: url(" . esc_url($bg_image) . ")'></div>";
 	}
 	echo "<div id='template-overlay' class='template-overlay'></div>";
 	?>
