26160b5a5a944493881a66f1f2eddfe09abf2467
BenSibley <ben@competethemes.com>
2014-10-29 10:22:51 -0400

    removed customizer JS for textures

--- js/customizer.js
+++ js/customizer.js
@@ -1,9 +1,5 @@
 jQuery(function($){
 
-    $('html', window.parent.document).find('#customize-control-ct_tracks_background_texture_setting input').each(function(){
-       $(this).parent().css('background-image', 'url("../wp-content/themes/tracks/assets/images/textures/' + $(this).val() + '.png")');
-    });
-
     // add multiple select styling
     $('#comment-display-control').multipleSelect({
         selectAll: false
