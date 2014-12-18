022a614f11f9095619c562e83c21920f87044b29
BenSibley <ben@competethemes.com>
2014-12-18 17:06:48 -0500

    added check for featured video existing before checking for 'both' value

--- content.php
+++ content.php
@@ -74,19 +74,25 @@ if( is_single() ) { ?>
     <div <?php post_class(); ?>>
 	    <?php
 
-	    // if post has video enabled on blog
-	    if( get_post_meta( $post->ID, 'ct_tracks_video_display_key', true ) == 'both' ) {
+        // check for Featured Video
+        $video = get_post_meta( $post->ID, 'ct_tracks_video_key', true );
 
-		    // check for Featured Video
-		    $video = get_post_meta( $post->ID, 'ct_tracks_video_key', true );
+        if( $video ) {
 
-		    // if has a video, embed it instead of featured image
-		    if ( $video ) {
-			    echo '<div class="featured-video">';
-			        echo wp_oembed_get( esc_url( $video ) );
-			    echo '</div>';
-		    }
-	    }
+            // if post has video enabled on blog
+            if ( get_post_meta( $post->ID, 'ct_tracks_video_display_key', true ) == 'both' ) {
+
+                // check for Featured Video
+                $video = get_post_meta( $post->ID, 'ct_tracks_video_key', true );
+
+                // if has a video, embed it instead of featured image
+                if ( $video ) {
+                    echo '<div class="featured-video">';
+                    echo wp_oembed_get( esc_url( $video ) );
+                    echo '</div>';
+                }
+            }
+        }
 	    // otherwise output Featured Image
 	    else {
 		    echo '<a class="featured-image-link" href="' . get_permalink() . '">';
