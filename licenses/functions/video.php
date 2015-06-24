497cfe9b815c5a54f7f4d32cd1714aa7f8243b98
BenSibley <ben@competethemes.com>
2015-06-24 15:14:12 -0400

    added blog only display option

--- licenses/functions/video.php
+++ licenses/functions/video.php
@@ -89,11 +89,15 @@ function ct_tracks_video_callback( $post ) {
 			echo '<p>' . __( 'Choose where to display the video:', 'tracks' ) . '</p>';
 			echo '<label for="ct_tracks_video_display_post">';
 				echo '<input type="radio" name="ct_tracks_video_display" id="ct_tracks_video_display_post" value="post" ' . checked( $display_value, "post", false ) . '>';
-				_e( 'Display on Post', 'tracks' );
+				_e( 'Post', 'tracks' );
+			echo '</label> ';
+			echo '<label for="ct_tracks_video_display_blog">';
+				echo '<input type="radio" name="ct_tracks_video_display" id="ct_tracks_video_display_blog" value="blog" ' . checked( $display_value, "blog", false ) . '>';
+				_e( 'Blog', 'tracks' );
 			echo '</label> ';
 			echo '<label for="ct_tracks_video_display_both">';
 				echo '<input type="radio" name="ct_tracks_video_display" id="ct_tracks_video_display_both" value="both" ' . checked( $display_value, "both", false ) . '>';
-				_e( 'Display on Post and Blog', 'tracks' );
+				_e( 'Post & Blog', 'tracks' );
 			echo '</label> ';
 		echo '</div>';
 	}
