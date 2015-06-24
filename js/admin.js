01a8d177e04d25c6920c526a1206286d930babdc
BenSibley <ben@competethemes.com>
2015-06-24 11:37:08 -0400

    added JS for dismissing notice

--- js/admin.js
+++ js/admin.js
@@ -75,4 +75,17 @@ jQuery(function($) {
             }
         });
     }
+
+    $('#tracks-avatar-notice').find('.notice-dismiss').bind('click', function(){
+
+        // set up data object
+        var data = {
+            action: 'dismiss_tracks_avatar_notice',
+            dismissed: true,
+            security: '<?php echo $ajax_nonce; ?>'
+        };
+
+        // post data received from PHP respond
+        jQuery.post(ajaxurl, data);
+    });
 });
