e1611904d70f5cf61ba63d48e23b8ca8929894b7
BenSibley <ben@competethemes.com>
2015-06-24 11:07:57 -0400

    comment edit link capitalized to match 'Reply' and i18n'ed

--- functions.php
+++ functions.php
@@ -109,7 +109,7 @@ if( ! function_exists( 'ct_tracks_customize_comments' ) ) {
                         'depth'      => $depth,
                         'max_depth'  => $args['max_depth']
                     ) ) ); ?>
-                    <?php edit_comment_link( 'edit' ); ?>
+                    <?php edit_comment_link( __('Edit', 'tracks') ); ?>
                 </div>
             </div>
             <div class="comment-content">
