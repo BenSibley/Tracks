3682b5d54dbcec14464fc9d9c00e7a315354795a
BenSibley <ben@competethemes.com>
2015-06-28 10:42:17 -0400

    selected bg texture now has blue outline, and textures lighten on hover

--- js/customizer.js
+++ js/customizer.js
@@ -4,6 +4,45 @@ jQuery(document).ready(function($) {
        $(this).parent().css('background-image', 'url("../wp-content/plugins/tracks-background-textures/textures/' + $(this).val() + '.png")');
     });
 
+    // set context to customizer panel outside iframe site content is in
+    var panel = $('html', window.parent.document);
+
+    addLayoutThumbnails();
+
+    // replaces radio buttons with images
+    function addLayoutThumbnails() {
+
+        // get layout inputs
+        var textureInputs = panel.find('#customize-control-ct_tracks_background_texture_setting').find('input');
+
+        // add the appropriate image to each label
+        textureInputs.each( function() {
+            $(this).parent().css('background-image', 'url("../wp-content/plugins/tracks-background-textures/textures/' + $(this).val() + '.png")');
+
+            // add initial 'selected' class
+            if ($(this).prop('checked')) {
+                $(this).parent().addClass('selected');
+            }
+        });
+
+        // watch for change of inputs (layouts)
+        panel.on('click', '#customize-control-ct_tracks_background_texture_setting input', function () {
+            addSelectedLayoutClass(textureInputs, $(this));
+        });
+    }
+
+    // add the 'selected' class when a new input is selected
+    function addSelectedLayoutClass(inputs, target) {
+
+        // remove 'selected' class from all labels
+        inputs.parent().removeClass('selected');
+
+        // apply 'selected' class to :checked input
+        if (target.prop('checked')) {
+            target.parent().addClass('selected');
+        }
+    }
+
     // add multiple select styling
     $('#comment-display-control').multipleSelect({
         selectAll: false
