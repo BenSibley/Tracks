6bf5e1500cca9fd449b093bb1f5d151a96bb3b2c
BenSibley <ben@competethemes.com>
2015-08-14 10:48:42 -0400

    removed menubar ARIA role from footer and secondary menus too

--- menu-footer.php
+++ menu-footer.php
@@ -2,7 +2,7 @@
 
 <div id="menu-footer" class="menu-container menu-footer" role="navigation">
 
-    <?php wp_nav_menu( array( 'theme_location' => 'footer', 'container_class' => 'menu', 'menu_class' => 'menu-footer-items', 'menu_id' => 'menu-footer-items', 'items_wrap' => '<ul id="%1$s" class="%2$s" role="menubar">%3$s</ul>', 'fallback_cb' => '') ); ?>
+    <?php wp_nav_menu( array( 'theme_location' => 'footer', 'container_class' => 'menu', 'menu_class' => 'menu-footer-items', 'menu_id' => 'menu-footer-items', 'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>', 'fallback_cb' => '') ); ?>
 
 </div><!-- #menu-footer .menu-container -->
 
--- menu-secondary.php
+++ menu-secondary.php
@@ -4,7 +4,7 @@
 
         <button id="toggle-secondary-navigation" class="toggle-secondary-navigation"><i class="fa fa-plus"></i></button>
 
-        <?php wp_nav_menu( array( 'theme_location' => 'secondary', 'container_class' => 'menu', 'menu_class' => 'menu-secondary-items', 'menu_id' => 'menu-secondary-items', 'items_wrap' => '<ul id="%1$s" class="%2$s" role="menubar">%3$s</ul>', 'fallback_cb' => '') ); ?>
+        <?php wp_nav_menu( array( 'theme_location' => 'secondary', 'container_class' => 'menu', 'menu_class' => 'menu-secondary-items', 'menu_id' => 'menu-secondary-items', 'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>', 'fallback_cb' => '') ); ?>
 
     </div><!-- #menu-secondary .menu-container -->
 
