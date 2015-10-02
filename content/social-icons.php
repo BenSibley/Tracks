85b030967b28264e0d0956e9781e7025b3b00da6
BenSibley <ben@competethemes.com>
2015-10-02 09:05:03 -0400

    simplified social icons markup

--- content/social-icons.php
+++ content/social-icons.php
@@ -5,6 +5,9 @@
 // array of social media site names
 $social_sites = ct_tracks_social_site_list();
 
+// icons that should use a special square icon
+$square_icons = array('linkedin', 'twitter', 'vimeo', 'youtube', 'pinterest', 'reddit', 'tumblr', 'steam', 'xing', 'github', 'google-plus', 'behance', 'facebook');
+
 // store social sites with input in array
 foreach( $social_sites as $social_site ) {
 	if( strlen( get_theme_mod( $social_site ) ) > 0 ) {
@@ -14,24 +17,33 @@ foreach( $social_sites as $social_site ) {
 
 // output markup for social icons
 if( ! empty( $active_sites ) ) {
+
 	echo '<ul class="social-media-icons">';
+
 		foreach ( $active_sites as $active_site ) {
-			echo '<li>';
-			// output link <a>
-			if( $active_site == 'email' ) {
-				echo '<a target="_blank" href="mailto:' . antispambot( is_email( get_theme_mod( $active_site ) ) ) . '">';
+
+			// get the square or plain class
+			if ( in_array( $active_site, $square_icons ) ) {
+				$class = 'fa fa-' . $active_site . '-square';
 			} else {
-				echo '<a target="_blank" href="' . esc_url( get_theme_mod( $active_site ) ) . '">';
+				$class = 'fa fa-' . $active_site;
 			}
-			// output icon <i>
-			if( $active_site ==  "flickr" || $active_site ==  "dribbble" || $active_site ==  "instagram" || $active_site ==  "soundcloud" || $active_site ==  "spotify" || $active_site ==  "vine" || $active_site ==  "yahoo" || $active_site ==  "codepen" || $active_site ==  "delicious" || $active_site ==  "stumbleupon" || $active_site ==  "deviantart" || $active_site ==  "digg" || $active_site ==  "hacker-news" || $active_site == 'vk' || $active_site == 'weibo' || $active_site == 'tencent-weibo') {
-				echo '<i class="fa fa-' . $active_site . '" title="' . sprintf( __("%s icon", "tracks"), $active_site ) . '"></i>';
-			} elseif( $active_site == 'email' ) {
-				echo '<i class="fa fa-envelope" title="' . sprintf( __("email icon", "tracks"), $active_site ) . '"></i>';
-			} else {
-				echo '<i class="fa fa-' . $active_site . '-square" title="' . sprintf( __("%s icon", "tracks"), $active_site ) . '"></i>';
+
+			if ( $active_site == 'email' ) {
+				?>
+				<li>
+					<a class="email" target="_blank" href="mailto:<?php echo antispambot( is_email( get_theme_mod( $active_site ) ) ); ?>">
+						<i class="fa fa-envelope" title="<?php _e('email icon', 'apex'); ?>"></i>
+					</a>
+				</li>
+			<?php } else { ?>
+				<li>
+					<a class="<?php echo $active_site; ?>" target="_blank" href="<?php echo esc_url( get_theme_mod( $active_site ) ); ?>">
+						<i class="<?php echo esc_attr( $class ); ?>" title="<?php printf( __('%s icon', 'apex'), $active_site ); ?>"></i>
+					</a>
+				</li>
+				<?php
 			}
-			echo '</a></li>';
 		}
 	echo "</ul>";
 }
