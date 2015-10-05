5e9df155c2d4593c13298d31c072d6d951bfcc8e
BenSibley <ben@competethemes.com>
2015-10-05 09:01:44 -0400

    category, tag, and author descriptions added to archive pages

--- content/archive-header.php
+++ content/archive-header.php
@@ -1,24 +1,26 @@
 <?php
 
 /* Category header */
-if(is_category()){ ?>
+if ( is_category() ) { ?>
 	<div class='archive-header'>
-	<p><?php _e('Category:', 'tracks'); ?></p>
+	<span><?php _e('Category:', 'tracks'); ?></span>
 	<h2><?php single_cat_title(); ?></h2>
+	<?php if ( category_description() ) echo category_description(); ?>
 	</div><?php
 }
 /* Tag header */
-elseif(is_tag()){ ?>
+elseif ( is_tag() ) { ?>
 	<div class='archive-header'>
-	<p><?php _e('Tag:', 'tracks'); ?></p>
+	<span><?php _e('Tag:', 'tracks'); ?></span>
 	<h2><?php single_tag_title(); ?></h2>
+	<?php if ( tag_description() ) echo tag_description(); ?>
 	</div><?php
 }
 /* Author header */
-elseif(is_author()){ ?>
+elseif ( is_author() ) { ?>
 	<div class='archive-header'>
-	<p><?php _e('These Posts are by:', 'tracks'); ?></p><?php
-	$author = get_userdata(get_query_var('author')); ?>
-	<h2><?php echo $author->nickname; ?></h2>
+	<span><?php _e('These Posts are by:', 'tracks'); ?></span>
+	<h2><?php the_author_meta( 'display_name' ); ?></h2>
+	<?php if ( get_the_author_meta( 'description' ) ) echo '<p>' . get_the_author_meta( 'description' ) . '</p>'; ?>
 	</div><?php
 }
