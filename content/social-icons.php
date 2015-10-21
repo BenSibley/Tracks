1416c777d93e4ba101daeb934d10aedf1a28162b
BenSibley <ben@competethemes.com>
2015-10-21 13:52:42 -0400

    added RSS to square icons

--- content/social-icons.php
+++ content/social-icons.php
@@ -6,7 +6,7 @@
 $social_sites = ct_tracks_social_site_list();
 
 // icons that should use a special square icon
-$square_icons = array('linkedin', 'twitter', 'vimeo', 'youtube', 'pinterest', 'reddit', 'tumblr', 'steam', 'xing', 'github', 'google-plus', 'behance', 'facebook');
+$square_icons = array('linkedin', 'twitter', 'vimeo', 'youtube', 'pinterest', 'rss', 'reddit', 'tumblr', 'steam', 'xing', 'github', 'google-plus', 'behance', 'facebook');
 
 // store social sites with input in array
 foreach( $social_sites as $social_site ) {
