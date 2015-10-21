f4ca4177f5628712eadb9bc8a27812bf4360e12c
BenSibley <ben@competethemes.com>
2015-10-21 13:46:41 -0400

    simplified post nav and now return to blog works with static homepage

--- content/further-reading.php
+++ content/further-reading.php
@@ -1,48 +1,67 @@
 <?php
 
-if( get_theme_mod('additional_options_further_reading_settings') != 'hide' ) {
+global $post;
 
-	global $post;
+// gets the previous post if it exists
+$previous_post = get_adjacent_post(false,'',true);
 
-	// gets the next & previous posts if they exist
-	$previous_blog_post = get_adjacent_post( false, '', true );
-	$next_blog_post     = get_adjacent_post( false, '', false );
-
-	if ( get_the_title( $previous_blog_post ) ) {
-		$previous_title = get_the_title( $previous_blog_post );
-	} else {
-		$previous_title = __( 'The Previous Post', 'tracks' );
-	}
-	if ( get_the_title( $next_blog_post ) ) {
-		$next_title = get_the_title( $next_blog_post );
-	} else {
-		$next_title = __( 'The Next Post', 'tracks' );
-	}
-
-	echo "<nav class='further-reading'>";
-
-	if ( $previous_blog_post ) {
-		echo "<p class='prev'>
-	            <span>" . __( 'Previous Post', 'tracks' ) . "</span>
-	            <a href='" . get_permalink( $previous_blog_post ) . "'>" . $previous_title . "</a>
-	        </p>";
-	} else {
-		echo "<p class='prev'>
-	            <span>" . __( 'This is the oldest post', 'tracks' ) . "</span>
-	            <a href='" . esc_url( home_url() ) . "'>" . __( 'Return to Blog', 'tracks' ) . "</a>
-	        </p>";
-	}
-	if ( $next_blog_post ) {
-
-		echo "<p class='next'>
-	            <span>" . __( 'Next Post', 'tracks' ) . "</span>
-	            <a href='" . get_permalink( $next_blog_post ) . "'>" . $next_title . "</a>
-	        </p>";
-	} else {
-		echo "<p class='next'>
-	            <span>" . __( 'This is the newest post', 'tracks' ) . "</span>
-	            <a href='" . esc_url( home_url() ) . "'>" . __( 'Return to Blog', 'tracks' ) . "</a>
-	         </p>";
-	}
-	echo "</nav>";
+// if there is a previous post
+if( $previous_post ) {
+	// text above the link
+	$previous_text = __('Previous Post', 'tracks');
+	// if there is a title use it, else call it "The Previous Post"
+	$previous_title = get_the_title( $previous_post ) ? get_the_title( $previous_post ) : __("The Previous Post", 'tracks');
+	// get the post link
+	$previous_link = get_permalink( $previous_post );
 }
+// if there isn't a previous post
+else {
+	// text above the link
+	$previous_text = __('No Older Posts', 'tracks');
+	// set the title to return to the blog
+	$previous_title = __('Return to Blog', 'tracks');
+	// link to blog
+	if( get_option( 'show_on_front' ) == 'page' ) {
+		$previous_link = get_permalink( get_option('page_for_posts' ) );
+	} else  {
+		$previous_link = get_home_url();
+	}
+}
+
+// gets the next post if it exists
+$next_post = get_adjacent_post(false,'',false);
+
+// if there is a next post
+if( $next_post ) {
+	// text above the link
+	$next_text = __('Next Post', 'tracks');
+	// if there is a title use it, else call it "The next Post"
+	$next_title = get_the_title( $next_post ) ? get_the_title( $next_post ) : __("The Next Post", 'tracks');
+	// get the post link
+	$next_link = get_permalink( $next_post );
+}
+// if there isn't a next post
+else {
+	// text above the link
+	$next_text = __('No Newer Posts', 'tracks');
+	// set the title to return to the blog
+	$next_title = __('Return to Blog', 'tracks');
+	// link to blog
+	if( get_option( 'show_on_front' ) == 'page' ) {
+		$next_link = get_permalink( get_option('page_for_posts' ) );
+	} else  {
+		$next_link = get_home_url();
+	}
+}
+
+?>
+<nav class="further-reading">
+	<p class="prev">
+		<span><?php echo esc_html( $previous_text ); ?></span>
+		<a href="<?php echo esc_url( $previous_link ); ?>"><?php echo esc_html( $previous_title ); ?></a>
+	</p>
+	<p class="next">
+		<span><?php echo esc_html( $next_text ); ?></span>
+		<a href="<?php echo esc_url( $next_link ); ?>"><?php echo esc_html( $next_title ); ?></a>
+	</p>
+</nav>
