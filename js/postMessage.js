acd7356c10fc00fdf4974b19898a5fee6fcadf55
BenSibley <ben@competethemes.com>
2015-10-22 13:40:14 -0400

    added postmessage support for site title

--- js/postMessage.js
+++ js/postMessage.js
@@ -1,3 +1,18 @@
-/**
- * Created by bensibley on 10/22/15.
- */
+( function( $ ) {
+
+    var panel = $('html', window.parent.document);
+    var body = $('body');
+    var siteTitle = $('#site-title');
+    var tagline = $( '.tagline' );
+
+    // Site title
+    wp.customize( 'blogname', function( value ) {
+        value.bind( function( to ) {
+            // if there is a logo, don't replace it
+            if( siteTitle.find('img').length == 0 ) {
+                siteTitle.children('a').text( to );
+            }
+        } );
+    } );
+
+} )( jQuery );
