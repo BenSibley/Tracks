40e76cec76d2b95b69913cc14af6a701e347fdbe
BenSibley <ben@competethemes.com>
2015-12-17 19:46:24 -0500

    implemented add_query_arg where relevant

--- theme-options.php
+++ theme-options.php
@@ -12,7 +12,7 @@ function ct_tracks_options_content(){
 	$customizer_url = add_query_arg(
 		array(
 			'url'    => site_url(),
-			'return' => admin_url('themes.php?page=tracks-options')
+            'return' => add_query_arg( 'page', 'tracks-options', admin_url( 'themes.php' ) )
 		),
 		admin_url('customize.php')
 	);
