3e07f4fad7249e391dd2fb69251918bb45aa9f75
BenSibley <ben@competethemes.com>
2015-12-19 15:19:35 -0500

    logic moved out of content-page for featured videos/images

--- content-page.php
+++ content-page.php
@@ -1,22 +1,5 @@
 <div class='entry'>
-	<?php
-	$video = get_post_meta( $post->ID, 'ct_tracks_video_key', true );
-
-	// if has a video, embed it instead of featured image
-	if ( $video ) {
-		echo '<div class="featured-video">';
-		echo wp_oembed_get( esc_url( $video ) );
-		echo '</div>';
-	} elseif ( get_theme_mod( 'premium_layouts_setting' ) == 'full-width-images' || get_theme_mod( 'premium_layouts_setting' ) == 'two-column-images' ) {
-		if ( has_post_thumbnail( $post->ID ) ) {
-			echo "<div class='featured-image-container'>";
-			ct_tracks_featured_image();
-			echo "</div>";
-		}
-	} else {
-		ct_tracks_featured_image();
-	}
-	?>
+	<?php ct_tracks_featured_image(); ?>
 	<div class='entry-header'>
 		<h1 class='entry-title'><?php the_title(); ?></h1>
 	</div>
