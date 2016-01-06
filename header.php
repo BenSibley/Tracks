b53660d0ab1997f543168da9c02139d8fc2df18b
BenSibley <ben@competethemes.com>
2016-01-06 09:11:47 -0500

    added hook for header image output

--- header.php
+++ header.php
@@ -9,18 +9,19 @@
 </head>
 
 <body id="<?php print get_stylesheet(); ?>" <?php body_class( 'ct-body' ); ?>>
-<div id="overflow-container" class="overflow-container">
-	<a class="skip-content" href="#main"><?php _e( 'Skip to content', 'tracks' ); ?></a>
-	<header id="site-header" class="site-header" role="banner">
-		<?php get_template_part( 'content/top-navigation' ); ?>
-		<div class="container">
-			<div id="title-info" class="title-info">
-				<?php get_template_part( 'logo' ) ?>
+	<?php do_action( 'tracks_body_top' ); ?>
+	<div id="overflow-container" class="overflow-container">
+		<a class="skip-content" href="#main"><?php _e( 'Skip to content', 'tracks' ); ?></a>
+		<header id="site-header" class="site-header" role="banner">
+			<?php get_template_part( 'content/top-navigation' ); ?>
+			<div class="container">
+				<div id="title-info" class="title-info">
+					<?php get_template_part( 'logo' ) ?>
+				</div>
+				<?php get_template_part( 'menu', 'primary' ); ?>
 			</div>
-			<?php get_template_part( 'menu', 'primary' ); ?>
-		</div>
-	</header>
-	<div id="main" class="main" role="main">
-<?php if ( function_exists( 'yoast_breadcrumb' ) ) {
-	yoast_breadcrumb( '<p id="breadcrumbs">', '</p>' );
-}
+		</header>
+		<div id="main" class="main" role="main">
+			<?php if ( function_exists( 'yoast_breadcrumb' ) ) {
+				yoast_breadcrumb( '<p id="breadcrumbs">', '</p>' );
+			}
