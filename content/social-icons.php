14907b288be4cacecd9b2b0e2746296ce09eb3ae
BenSibley <ben@competethemes.com>
2016-04-07 10:57:59 -0400

    corrected use of esc_attr_e in social icons

--- content/social-icons.php
+++ content/social-icons.php
@@ -42,7 +42,7 @@ if ( ! empty( $active_sites ) ) {
 					<a class="email" target="_blank"
 					   href="mailto:<?php echo antispambot( is_email( get_theme_mod( $active_site ) ) ); ?>">
 						<i class="fa fa-envelope" title="<?php esc_attr_e( 'email', 'tracks' ); ?>"></i>
-						<span class="screen-reader-text"><?php _e( 'email', 'tracks' ); ?></span>
+						<span class="screen-reader-text"><?php esc_attr_e( 'email', 'tracks' ); ?></span>
 					</a>
 				</li>
 			<?php } elseif ( $active_site == 'email-form' ) { ?>
@@ -50,15 +50,15 @@ if ( ! empty( $active_sites ) ) {
 					<a class="contact-form" target="_blank"
 					   href="<?php echo esc_url( get_theme_mod( $active_site ) ); ?>">
 						<i class="fa fa-envelope-o" title="<?php esc_attr_e( 'contact form', 'tracks' ); ?>"></i>
-						<span class="screen-reader-text"><?php _e( 'contact form', 'tracks' ); ?></span>
+						<span class="screen-reader-text"><?php esc_attr_e( 'contact form', 'tracks' ); ?></span>
 					</a>
 				</li>
 			<?php }  elseif ( $active_site == 'skype') { ?>
 				<li>
 					<a class="<?php echo esc_attr( $active_site ); ?>" target="_blank"
 					   href="<?php echo esc_url( get_theme_mod( $active_site ), array( 'http', 'https', 'skype' ) ); ?>">
-						<i class="<?php echo esc_attr( $class ); ?>" title="<?php esc_attr_e( $active_site ); ?>"></i>
-						<span class="screen-reader-text"><?php esc_attr_e( $active_site ); ?></span>
+						<i class="<?php echo esc_attr( $class ); ?>" title="<?php echo esc_attr( $active_site ); ?>"></i>
+						<span class="screen-reader-text"><?php echo esc_attr( $active_site ); ?></span>
 					</a>
 				</li>
 				<?php
@@ -66,8 +66,8 @@ if ( ! empty( $active_sites ) ) {
 				<li>
 					<a class="<?php echo esc_attr( $active_site ); ?>" target="_blank"
 					   href="<?php echo esc_url( get_theme_mod( $active_site ) ); ?>">
-						<i class="<?php echo esc_attr( $class ); ?>" title="<?php esc_attr_e( $active_site ); ?>"></i>
-						<span class="screen-reader-text"><?php esc_attr_e( $active_site ); ?></span>
+						<i class="<?php echo esc_attr( $class ); ?>" title="<?php echo esc_attr( $active_site ); ?>"></i>
+						<span class="screen-reader-text"><?php echo esc_attr( $active_site ); ?></span>
 					</a>
 				</li>
 				<?php
