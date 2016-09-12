01457e24d68e06110ba3879c1c1f08eb207f9678
BenSibley <ben@competethemes.com>
2016-09-12 09:42:27 -0400

    comment nesting can now be turned off

--- comments.php
+++ comments.php
@@ -24,7 +24,7 @@ if ( comments_open() ) { ?>
 			</h2>
 		</div>
 		<ol class="comment-list">
-			<?php wp_list_comments( array( 'callback' => 'ct_tracks_customize_comments', 'max_depth' => '7' ) ); ?>
+			<?php wp_list_comments( array( 'callback' => 'ct_tracks_customize_comments' ) ); ?>
 		</ol>
 		<?php
 		if ( ( get_option( 'page_comments' ) == 1 ) && ( get_comment_pages_count() > 1 ) ) { ?>
@@ -44,7 +44,7 @@ if ( comments_open() ) { ?>
 			</h2>
 		</div>
 		<ol class="comment-list">
-			<?php wp_list_comments( array( 'callback' => 'ct_tracks_customize_comments', 'max_depth' => '3' ) ); ?>
+			<?php wp_list_comments( array( 'callback' => 'ct_tracks_customize_comments' ) ); ?>
 		</ol>
 		<?php
 		if ( ( get_option( 'page_comments' ) == 1 ) && ( get_comment_pages_count() > 1 ) ) { ?>
@@ -66,7 +66,7 @@ if ( comments_open() ) { ?>
 			</h2>
 		</div>
 		<ol class="comment-list">
-			<?php wp_list_comments( array( 'callback' => 'ct_tracks_customize_comments', 'max_depth' => '3' ) ); ?>
+			<?php wp_list_comments( array( 'callback' => 'ct_tracks_customize_comments' ) ); ?>
 		</ol>
 		<?php
 		if ( ( get_option( 'page_comments' ) == 1 ) && ( get_comment_pages_count() > 1 ) ) { ?>
