cf5111b14c3672d87e10901c04b0743c5b1d2694
BenSibley <ben@competethemes.com>
2017-08-10 15:29:00 -0400

    Fix for Facebook descriptions when using Yoast SEO

--- functions.php
+++ functions.php
@@ -260,6 +260,18 @@ if ( ! function_exists( 'ct_tracks_remove_more_link_scroll' ) ) {
 }
 add_filter( 'the_content_more_link', 'ct_tracks_remove_more_link_scroll' );
 
+// Yoast OG description has "Read the postTitle of the Post" due to its use of get_the_excerpt(). This fixes that.
+function ct_tracks_update_yoast_og_description( $ogdesc ) {
+	$read_more_text = get_theme_mod( 'read_more_text' );
+	if ( empty( $read_more_text ) ) {
+		$read_more_text = __( 'Read the post', 'tracks' );
+	}
+	$ogdesc = substr( $ogdesc, 0, strpos( $ogdesc, $read_more_text ) );
+
+	return $ogdesc;
+}
+add_filter( 'wpseo_opengraph_desc', 'ct_tracks_update_yoast_og_description' );
+
 // for displaying featured images including mobile versions and default versions
 if ( ! function_exists( 'ct_tracks_featured_image' ) ) {
 	function ct_tracks_featured_image() {
